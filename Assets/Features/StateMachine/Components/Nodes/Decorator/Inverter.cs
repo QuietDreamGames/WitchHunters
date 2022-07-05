352c1446e5895c902f020d814c2cecfdb0cba15c
Volcov Oleg <bojikch15@gmail.com>
2022-07-05 19:15:31 +0300

    Create new state machine inverter node behaviour.

---
+++ Assets/Features/StateMachine/Components/Nodes/Decorator/Inverter.cs
@@ -0,0 +1,10 @@
+﻿using Features.StateMachine.Components.Core;
+using Unity.Entities;
+
+namespace Features.StateMachine.Components.Nodes.Decorator
+{
+    public struct Inverter : IComponentData, INode
+    {
+        
+    }
+}
