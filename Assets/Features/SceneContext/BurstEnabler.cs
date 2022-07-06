a360a45e3142a7ddd2756a284985380512ec83f5
Volcov Oleg <bojikch15@gmail.com>
2022-07-06 18:59:26 +0300

    Create editor script to enable/disable burst compilation.

---
+++ Assets/Features/SceneContext/BurstEnabler.cs
@@ -0,0 +1,22 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+
+namespace Features.SceneContext
+{
+    public class BurstEnabler : Editor
+    {
+        [MenuItem("Utility/Burst Options/Enable Burst")]
+        public static void EnableBurst()
+        {
+            EditorPrefs.SetBool("BurstCompilation", true);
+        }
+        
+        [MenuItem("Utility/Burst Options/Disable Burst")]
+        public static void DisableBurst()
+        {
+            EditorPrefs.SetBool("BurstCompilation", false);
+        }
+    }
+}
+
+#endif
