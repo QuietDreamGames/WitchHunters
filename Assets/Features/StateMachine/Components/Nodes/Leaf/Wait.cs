bc859f00e8c6da208aeddd4b51da68f50ca61a1e
Volcov Oleg <bojikch15@gmail.com>
2022-07-06 18:03:37 +0300

    Create new wait and log state machine action nodes.

---
+++ Assets/Features/StateMachine/Components/Nodes/Leaf/Wait.cs
@@ -0,0 +1,17 @@
+﻿using Features.StateMachine.Components.Core;
+using Unity.Entities;
+
+namespace Features.StateMachine.Components.Nodes.Leaf
+{
+    public struct Wait : IComponentData, INode
+    {
+        public readonly float WaitTime;
+
+        public float CurrentWaitTime;
+
+        public Wait(float waitTime) : this()
+        {
+            WaitTime = waitTime;
+        }
+    }
+}
---
+++ Assets/Features/StateMachine/Services/Core/JobLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Unity.Burst;
+using Unity.Collections;
+using UnityEngine;
+using LogType = Features.StateMachine.Components.Nodes.Leaf.LogType;
+
+namespace Features.StateMachine.Services.Core
+{
+    public static class JobLogger
+    {
+        [BurstDiscard] 
+        public static void Log(FixedString32Bytes message, LogType type)
+        {
+            switch (type)
+            {
+                case LogType.Simple:
+                    Debug.Log(message);
+                    break;
+                case LogType.Warning:
+                    Debug.LogWarning(message);
+                    break;
+                case LogType.Error:
+                    Debug.LogError(message);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(type), type, null);
+            }
+        }
+    }
+}
