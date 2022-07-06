ae98bbc9fb38aba0b9175385a71af99afb5b7e16
Volcov Oleg <bojikch15@gmail.com>
2022-07-06 18:04:45 +0300

    Create new state machine parallel node.

---
+++ Assets/Features/StateMachine/Components/Nodes/Composite/Parallel.cs
@@ -0,0 +1,25 @@
+﻿using Features.StateMachine.Components.Core;
+using Unity.Entities;
+
+namespace Features.StateMachine.Components.Nodes.Composite
+{
+    public struct Parallel : IComponentData, INode
+    {
+        public readonly int NodeCount;
+		
+        public readonly ParallelType Type;
+
+        public Parallel(int nodeCount, ParallelType type) : this()
+        {
+            NodeCount = nodeCount;
+            Type = type;
+        } 
+    }
+
+    public enum ParallelType
+    {
+        Simple = 0,
+        Sequence,
+        Selector
+    }
+}
---
+++ Assets/Features/StateMachine/Systems/Nodes/Composite/ParallelSystem.cs
@@ -0,0 +1,160 @@
+﻿using Features.StateMachine.Components.Core;
+using Features.StateMachine.Components.Nodes.Composite;
+using Features.StateMachine.Systems.Core;
+using Features.StateMachine.Systems.Nodes.Composite;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Entities;
+using Unity.Jobs;
+
+[assembly: RegisterGenericJobType(typeof(ParallelSystem.ExecuteNodesJob<Parallel, ParallelSystem.Processor>))]
+namespace Features.StateMachine.Systems.Nodes.Composite
+{
+    public partial class ParallelSystem : NodeExecutorSystem<Parallel, ParallelSystem.Processor>
+	{
+		protected override Processor PrepareProcessor()
+		{
+			var allNodesQuery = GetEntityQuery(typeof(NodeComponent));
+			
+			return new Processor
+			{
+				NodeComponentType = GetComponentTypeHandle<NodeComponent>(),
+				AllNodesChunks = allNodesQuery.CreateArchetypeChunkArray(Allocator.TempJob),
+			};
+		}
+
+		public struct Processor : INodeProcessor<Parallel>
+		{
+			[NativeDisableContainerSafetyRestriction]
+			public ComponentTypeHandle<NodeComponent> NodeComponentType;
+			
+			[ReadOnly]
+			[DeallocateOnJobCompletion]
+			public NativeArray<ArchetypeChunk> AllNodesChunks;
+
+			public void BeforeChunkIteration(ArchetypeChunk batchInChunk, int batchIndex)
+            {
+	            
+            }
+
+			public NodeResult Start(in Entity rootEntity,
+				in Entity parallelEntity,
+				ref Parallel parallelComponent,
+				int indexOfFirstEntityInQuery,
+				int iterIndex)
+			{
+				Runner(in parallelEntity);
+
+				return NodeResult.Running;
+			}
+
+			public NodeResult Update(in Entity rootEntity,
+				in Entity parallelEntity,
+				ref Parallel parallelComponent,
+				int indexOfFirstEntityInQuery,
+				int iterIndex)
+			{
+				var result = GetResult(in parallelEntity, in parallelComponent);
+
+				if (result is NodeResult.Success or NodeResult.Failed)
+				{
+					StopChildren(in parallelEntity);
+				}
+
+				return result;
+			}
+
+			private void Runner(in Entity parallelEntity)
+			{
+				for (int j = 0; j < AllNodesChunks.Length; j++)
+				{
+					var batchInChunk = AllNodesChunks[j];
+					var allNodes = batchInChunk.GetNativeArray(NodeComponentType);
+
+					for (int i = 0; i < allNodes.Length; i++)
+					{
+						var node = allNodes[i];
+                					
+						if (node.AgentEntity != parallelEntity)
+							continue;
+						
+						node.IsExec = true;
+						node.Started = false;
+						node.Result = NodeResult.Running;
+
+						allNodes[i] = node;
+					}
+				}
+			}
+
+			private void StopChildren(in Entity parallelEntity)
+			{
+				for (int j = 0; j < AllNodesChunks.Length; j++)
+				{
+					var batchInChunk = AllNodesChunks[j];
+					var allNodes = batchInChunk.GetNativeArray(NodeComponentType);
+
+					for (int i = 0; i < allNodes.Length; i++)
+					{
+						var node = allNodes[i];
+                					
+						if (node.AgentEntity != parallelEntity)
+							continue;
+						
+						node.IsExec = false;
+						node.Started = false;
+
+						allNodes[i] = node;
+					}
+				}
+			}
+			
+			private NodeResult GetResult(in Entity parallelEntity, in Parallel parallelComponent)
+			{
+				var isAnyChildRunning = false;
+				
+				for (int j = 0; j < AllNodesChunks.Length; j++)
+				{
+					var batchInChunk = AllNodesChunks[j];
+					var allNodes = batchInChunk.GetNativeArray(NodeComponentType);
+
+					for (int i = 0; i < allNodes.Length; i++)
+					{
+						var node = allNodes[i];
+                					
+						if (node.AgentEntity != parallelEntity)
+							continue;
+						
+						if (node.Result is NodeResult.Failed)
+						{
+							if (parallelComponent.Type is ParallelType.Sequence)
+							{
+								return NodeResult.Failed;
+							}
+						}
+						
+						if (node.Result is NodeResult.Success)
+						{
+							if (parallelComponent.Type is ParallelType.Selector)
+							{
+								return NodeResult.Success;
+							}
+						}
+
+						if (node.Result is NodeResult.Running)
+						{
+							if (isAnyChildRunning == false)
+							{
+								isAnyChildRunning = true;
+							}
+						}
+					}
+				}
+
+				return isAnyChildRunning 
+					? NodeResult.Running 
+					: NodeResult.Success;
+			}
+		}
+	}
+}
