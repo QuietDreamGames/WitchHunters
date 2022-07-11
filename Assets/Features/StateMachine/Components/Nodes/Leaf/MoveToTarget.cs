d8d8583a392691b0d23a253b4b86519f82fa1de2
Volcov Oleg <bojikch15@gmail.com>
2022-07-11 21:57:55 +0300

    Create new move to target node to move any character to target after getting target position.

---
+++ Assets/Features/StateMachine/Components/Nodes/Leaf/MoveToTarget.cs
@@ -0,0 +1,19 @@
+﻿using Features.StateMachine.Components.Core;
+using Unity.Burst;
+using Unity.Entities;
+
+namespace Features.StateMachine.Components.Nodes.Leaf
+{
+    [BurstCompile]
+    public struct MoveToTarget : IComponentData, INode
+    {
+        public readonly float AcceptableRadius;
+        public readonly float Speed;
+
+        public MoveToTarget(float acceptableRadius, float speed) : this()
+        {
+            AcceptableRadius = acceptableRadius;
+            Speed = speed;
+        }
+    }
+}
