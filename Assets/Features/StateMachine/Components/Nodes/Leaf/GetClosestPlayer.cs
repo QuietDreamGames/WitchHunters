262b793056f68a4dbd2b6f70638594685a20bce3
Volcov Oleg <bojikch15@gmail.com>
2022-07-11 21:57:14 +0300

    Create new get closest player node for enemy sensor behaviour.

---
+++ Assets/Features/StateMachine/Components/Nodes/Leaf/GetClosestPlayer.cs
@@ -0,0 +1,17 @@
+﻿using Features.StateMachine.Components.Core;
+using Unity.Burst;
+using Unity.Entities;
+
+namespace Features.StateMachine.Components.Nodes.Leaf
+{
+    [BurstCompile]
+    public struct GetClosestPlayer : IComponentData, INode
+    {
+        public readonly float Distance;
+
+        public GetClosestPlayer(float distance) : this()
+        {
+            Distance = distance;
+        }
+    }
+}
