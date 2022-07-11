d0af41aff5a34d2a0a497e2695bc7eb291a60074
Volcov Oleg <bojikch15@gmail.com>
2022-07-11 23:55:11 +0300

    Create new node to choose and play single animation with play next animation system.

---
+++ Assets/Features/StateMachine/Components/Nodes/Leaf/PlayAnimation.cs
@@ -0,0 +1,18 @@
+﻿using Features.StateMachine.Components.Core;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace Features.StateMachine.Components.Nodes.Leaf
+{
+    [BurstCompile]
+    public struct PlayAnimation : IComponentData, INode
+    {
+        public readonly FixedString32Bytes AnimationName;
+
+        public PlayAnimation(string animationName) : this()
+        {
+            AnimationName = animationName;
+        }
+    }
+}
