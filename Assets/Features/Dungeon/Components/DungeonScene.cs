4b20c1fa0d2d61f560823dc66dfdaecdd3780c95
Volcov Oleg <bojikch15@gmail.com>
2022-07-31 21:02:13 +0300

    Create new components for dungeon generation in runtime.

---
+++ Assets/Features/Dungeon/Components/DungeonScene.cs
@@ -0,0 +1,23 @@
+﻿using Edgar.Unity;
+using Unity.Entities;
+
+namespace Features.Dungeon.Components
+{
+    public class DungeonScene : IComponentData
+    {
+        public DungeonGeneratorGrid2D Grid;
+        public bool Generated;
+
+        public DungeonScene()
+        {
+            Grid = default;
+            Generated = false;
+        }
+
+        public DungeonScene(DungeonGeneratorGrid2D grid)
+        {
+            Grid = grid;
+            Generated = false;
+        }
+    }
+}
