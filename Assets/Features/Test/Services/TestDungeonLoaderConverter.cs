4995fb66618f283c3e92f699ddcaa3da84aab8bc
Volcov Oleg <bojikch15@gmail.com>
2022-07-31 21:04:48 +0300

    Create converters to set data for dungeon generator system.

---
+++ Assets/Features/Test/Services/TestDungeonLoaderConverter.cs
@@ -0,0 +1,18 @@
+﻿using Edgar.Unity;
+using Features.Dungeon.Components;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Features.Test.Services
+{
+    public class TestDungeonLoaderConverter : MonoBehaviour, IConvertGameObjectToEntity
+    {
+        [SerializeField] private LevelGraph _levelGraph;
+        
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+        {
+            var dungeonLoader = new DungeonLoader(_levelGraph);
+            dstManager.AddComponentData(entity, dungeonLoader);
+        }
+    }
+}
