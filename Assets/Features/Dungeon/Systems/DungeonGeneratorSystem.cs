e644b6819f0da990bd60a86b4a0e2a525980a2c7
Volcov Oleg <bojikch15@gmail.com>
2022-07-31 21:03:58 +0300

    Create new system to configure and generate dungeon.

---
+++ Assets/Features/Dungeon/Systems/DungeonGeneratorSystem.cs
@@ -0,0 +1,42 @@
+﻿using Features.Dungeon.Components;
+using Unity.Entities;
+
+namespace Features.Dungeon.Systems
+{
+    public partial class DungeonGeneratorSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            RequireSingletonForUpdate<DungeonScene>();
+            if (!HasSingleton<DungeonScene>())
+            {
+                return;
+            }
+
+            var dungeonScene = GetComponentFromSingleton<DungeonScene>();
+            if (dungeonScene.Generated)
+                return;
+            
+            RequireSingletonForUpdate<DungeonLoader>();
+            if (!HasSingleton<DungeonLoader>())
+            {
+                return;
+            }
+            
+            var dungeonLoader = GetComponentFromSingleton<DungeonLoader>();
+            var dungeonGenerator = dungeonScene.Grid;
+
+            dungeonGenerator.FixedLevelGraphConfig.LevelGraph = dungeonLoader.LevelGraph;
+            dungeonGenerator.Generate();
+            
+            dungeonScene.Generated = true;
+        }
+
+        private T GetComponentFromSingleton<T>() where T : class, IComponentData
+        {
+            var entity = GetSingletonEntity<T>();
+            var component = EntityManager.GetComponentData<T>(entity);
+            return component;
+        } 
+    }
+}
