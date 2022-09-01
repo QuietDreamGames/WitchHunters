5aa158dfd6bea8aabf9f5df450a88610edbfd35c
Odinlevi <odinlevi@gmail.com>
2022-09-01 19:45:15 +0300

    Add Gameplay dialog panel with adjustable text components inside

---
+++ Assets/Features/UI/Services/ClickableText.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Features.UI.Services
+{
+    public class ClickableText : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+    {
+        [SerializeField] private TextMeshProUGUI _text;
+        [SerializeField] private Color _selectedColor;
+        [SerializeField] private Color _unselectedColor;
+        [SerializeField] private Color _downColor;
+        
+        public Action OnClick;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            OnClick?.Invoke();
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _text.color = _selectedColor;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            _text.color = _unselectedColor;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            _text.color = _downColor;
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            _text.color = _selectedColor;
+        }
+    }
+}
