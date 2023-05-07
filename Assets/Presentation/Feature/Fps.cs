dc13ebe1252beef30415e493315de3fc20913c96
odinlevi@gmail.com <maxim.nichiforov.tekedu@gmail.com>
2023-05-07 20:16:33 +0300

    Add separate FPS and average FPS counters.

--- Assets/Presentation/Feature/Fps.cs
+++ Assets/Presentation/Feature/Fps.cs
@@ -1,25 +1,44 @@
+using System;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 namespace Presentation.Feature
 {
     public class Fps : MonoBehaviour
     {
-        private float count;
-    
-        private IEnumerator Start()
+        [SerializeField] private TextMeshProUGUI _fpsCounter;
+        [SerializeField] private TextMeshProUGUI _avgFpsCounter;
+        
+        private float deltaTime = 0.0f;
+        private float totalFrameTime = 0.0f;
+        private float avgFps;
+        private int count = 1;
+
+        private void Start()
         {
-            GUI.depth = 2;
-            while (true)
-            {
-                count = 1f / Time.deltaTime;
-                yield return new WaitForSeconds(0.1f);
-            }
+            avgFps = 1.0f / deltaTime;
+            Recalculate();
         }
-    
-        private void OnGUI()
+
+        private void Update()
         {
-            GUI.Label(new Rect(5, 40, 100, 25), "FPS: " + Mathf.Round(count));
+            Recalculate();
         }
+
+        private void  Recalculate()
+        {
+            deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+            
+            float msec = deltaTime * 1000.0f;
+            float fps = 1.0f / deltaTime;
+            count++;
+            totalFrameTime += fps;
+            avgFps = totalFrameTime / count;
+
+            _fpsCounter.text = $"Ms: {msec}, FPS: {fps}";
+            _avgFpsCounter.text = $"Average FPS: {avgFps}";
+        }
+        
     }
 }
