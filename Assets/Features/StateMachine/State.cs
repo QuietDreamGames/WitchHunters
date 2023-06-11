a9c069b75d3004f211c59d12cb8e61b577c4b19f
odinlevi@gmail.com <odinlevi@gmail.com>
2023-06-11 15:16:25 +0300

    Implementation of basic State Machine

---
+++ Assets/Features/StateMachine/State.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+namespace Features.StateMachine
+{
+    public abstract class State
+    {
+        protected float time { get; set; }
+        protected float fixedTime { get; set; }
+        protected float lateTime { get; set; }
+        
+        public StateMachine StateMachine;
+        
+        public virtual void OnEnter()
+        {
+            
+        }
+        
+        public virtual void OnEnter(StateMachine stateMachine)
+        {
+            StateMachine = stateMachine;
+        }
+        
+        public virtual void OnUpdate()
+        {
+            time += Time.deltaTime;
+        }
+        
+        public virtual void OnFixedUpdate()
+        {
+            fixedTime += Time.deltaTime;
+        }
+        
+        public virtual void OnLateUpdate()
+        {
+            lateTime += Time.deltaTime;
+        }
+        
+        public virtual void OnExit()
+        {
+            
+        }
+
+        #region Passthrough methods
+        
+        protected static void Destroy(Object obj)
+        {
+            Object.Destroy(obj);
+        }
+        
+        protected T GetComponent<T>() where T : Component
+        {
+            return StateMachine.GetComponent<T>();
+        }
+        
+        protected Component GetComponent(System.Type type)
+        {
+            return StateMachine.GetComponent(type);
+        }
+
+        protected Component GetComponent(string type)
+        {
+            return StateMachine.GetComponent(type);
+        }
+        
+        #endregion
+    }
+}
---
+++ Assets/Features/StateMachine/StateMachine.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+namespace Features.StateMachine
+{
+    public class StateMachine : MonoBehaviour
+    {
+        public State CurrentState { get; private set; }
+        private State _nextState;
+
+        private void Update()
+        {
+            if (_nextState != null)
+            {
+                SetState(_nextState);
+                _nextState = null;
+            }
+            
+            CurrentState.OnUpdate();
+        }
+        
+        private void FixedUpdate()
+        {
+            CurrentState.OnFixedUpdate();
+        }
+        
+        private void LateUpdate()
+        {
+            CurrentState.OnLateUpdate();
+        }
+        
+        private void SetState(State state)
+        {
+            if (CurrentState != null)
+            {
+                CurrentState.OnExit();
+            }
+            
+            CurrentState = state;
+            CurrentState.OnEnter(this);
+        }
+        
+        public void SetNextState(State state)
+        {
+            if (state != null)
+                
+                _nextState = state;
+        }
+    }
+}
