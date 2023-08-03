0816150a3e0f61cba912b1227d206f5dbdf683ff
odinlevi@gmail.com <odinlevi@gmail.com>
2023-08-03 17:56:24 +0300

    Gameplay Canvas is made a prefab.

---
+++ Assets/Features/Experience/ExperienceController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Features.SaveSystems.Interfaces;
+using UnityEngine;
+
+namespace Features.Experience
+{
+    public class ExperienceController : MonoBehaviour, ISavable
+    {
+        [SerializeField] private ExpData _expData;
+        
+        public ISavableSerializer Serializer { get; set; }
+        public byte[] Save()
+        {
+            return Serializer.Serialize(_expData);
+        }
+
+        public void Load(byte[] data)
+        {
+            _expData = Serializer.Deserialize<ExpData>(data);
+        }
+    }
+
+    [Serializable]
+    public class ExpData
+    {
+        public int expAmount;
+    }
+}
