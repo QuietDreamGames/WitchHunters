6739b9aa60834bed3bc1cf23a892eb7ce5043783
Volcov Oleg <bojikch15@gmail.com>
2023-08-03 18:49:44 +0300

    Refactor room decor layout.

---
+++ Assets/Features/Dungeons/Initialization/VersionChooser.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Features.Dungeons.Initialization
+{
+    public class VersionChooser : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
