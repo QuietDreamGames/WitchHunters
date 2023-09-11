42681c7841bb5adeeb367a5c579923ca9a2abbf1
odinlevi@gmail.com <odinlevi@gmail.com>
2023-09-11 18:16:26 +0300

    Adjusted the chances logic.

--- Assets/Features/Drop/DropData.cs
+++ Assets/Features/Drop/DropData.cs
@@ -25,73 +25,82 @@ namespace Features.Drop
         public InventoryItem GetRandomDrop()
         {
             var random = UnityEngine.Random.Range(0f, 1f);
-            // var currentChance = 0f;
+            Debug.Log($"random: {random}");
+            var currentChance = 1f;
             foreach (var drop in possibleDrops)
             {
-                // currentChance += drop.dropChance;
-                if (random < drop.dropChance) continue;
-                    switch (drop.itemData.sortType)
-                    {
-                        case ItemSortType.Armor:
-                            var newItem = new EquippableItem
-                            {
-                                amount = 1,
-                                isEquipped = false,
-                                itemData = drop.itemData,
-                            };
-                                
-                            newItem.ResetModifiers();
+                currentChance -= drop.dropChance;
+                if (random < currentChance) continue;
+                
+                switch (drop.itemData.sortType)
+                {
+                    case ItemSortType.Armor:
+                        var newItem = new EquippableItem
+                        {
+                            amount = 1,
+                            isEquipped = false,
+                            itemData = drop.itemData,
+                        };
                             
-                            for (int i = 0; i < possibleArmorModifiers.Length; i++)
-                            {
-                                var modifier = possibleArmorModifiers[i];
-                                if (random < modifier.dropChance) continue;
+                        newItem.ResetModifiers();
+                        
+                        var modifierRandom = UnityEngine.Random.Range(0f, 1f);
+                        Debug.Log($"modifierRandom: {modifierRandom}");
+                        var modifierCurrentChance = 1f;
+                        for (int i = 0; i < possibleArmorModifiers.Length; i++)
+                        {
+                            var modifier = possibleArmorModifiers[i];
+                            modifierCurrentChance -= modifier.dropChance;
+                            if (modifierRandom < modifierCurrentChance) continue;
 
-                                var itemModifier = new ItemModifier
-                                {
-                                    modifierData = modifier.modifierData,
-                                    modifierType = modifier.modifierType
-                                };
-                                
-                                newItem.itemModifiers.Add(itemModifier);
-                                break;
-                            }
-                            break;
-                        case ItemSortType.Jewelry:
-                            newItem = new EquippableItem
+                            var itemModifier = new ItemModifier
                             {
-                                amount = 1,
-                                isEquipped = false,
-                                itemData = drop.itemData,
+                                modifierData = modifier.modifierData,
+                                modifierType = modifier.modifierType
                             };
-                                
-                            newItem.ResetModifiers();
                             
-                            for (int i = 0; i < possibleJewelryModifiers.Length; i++)
-                            {
-                                var modifier = possibleJewelryModifiers[i];
-                                if (random < modifier.dropChance) continue;
+                            newItem.itemModifiers.Add(itemModifier);
+                            return newItem;
+                        }
+                        return newItem;
+                    case ItemSortType.Jewelry:
+                        newItem = new EquippableItem
+                        {
+                            amount = 1,
+                            isEquipped = false,
+                            itemData = drop.itemData,
+                        };
+                            
+                        newItem.ResetModifiers();
+                        
+                        modifierRandom = UnityEngine.Random.Range(0f, 1f);
+                        modifierCurrentChance = 1f;
+                        for (int i = 0; i < possibleJewelryModifiers.Length; i++)
+                        {
+                            var modifier = possibleJewelryModifiers[i];
+                            modifierCurrentChance -= modifier.dropChance;
+                            if (modifierRandom < modifierCurrentChance) continue;
 
-                                var itemModifier = new ItemModifier
-                                {
-                                    modifierData = modifier.modifierData,
-                                    modifierType = modifier.modifierType
-                                };
-                                
-                                newItem.itemModifiers.Add(itemModifier);
-                                break;
-                            }
-                            
-                            break;
-                        case ItemSortType.Scrap:
-                        case ItemSortType.Quest:
-                            return new InventoryItem
+                            var itemModifier = new ItemModifier
                             {
-                                amount = 1,
-                                itemData = drop.itemData
+                                modifierData = modifier.modifierData,
+                                modifierType = modifier.modifierType
                             };
-                        default:
-                            throw new ArgumentOutOfRangeException();
+                            
+                            newItem.itemModifiers.Add(itemModifier);
+                            return newItem;
+                        }
+                        
+                        return newItem;
+                    case ItemSortType.Scrap:
+                    case ItemSortType.Quest:
+                        return new InventoryItem
+                        {
+                            amount = 1,
+                            itemData = drop.itemData
+                        };
+                    default:
+                        throw new ArgumentOutOfRangeException();
                     }
             }
 
