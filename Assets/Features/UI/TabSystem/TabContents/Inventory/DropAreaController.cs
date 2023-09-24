a56d06db5a9fc7b456e9758aef8322d7ac9fc440
odinlevi@gmail.com <odinlevi@gmail.com>
2023-09-24 16:19:06 +0300

    Drop Items from inventory procedure.

--- Assets/Features/UI/TabSystem/TabContents/Inventory/DropAreaController.cs
+++ Assets/Features/UI/TabSystem/TabContents/Inventory/DropAreaController.cs
@@ -3,20 +3,25 @@ using UnityEngine.EventSystems;
 
 namespace Features.UI.TabSystem.TabContents.Inventory
 {
-    public class DropAreaController : MonoBehaviour, IDropHandler
+    public abstract class DropAreaController : MonoBehaviour, IDropHandler
     {
         protected DraggableItemController draggableItemController;
+        protected IRemoveItemProcessor removeItemProcessor;
+        
+        public void Initiate(IRemoveItemProcessor removeItemProcessor)
+        {
+            draggableItemController = null;
+            this.removeItemProcessor = removeItemProcessor;
+        }
+        
         
         public void OnDrop(PointerEventData eventData)
         {
             draggableItemController = eventData.pointerDrag.GetComponent<DraggableItemController>();
 
-            ProcessDrop();
+            ProcessSingleDrop();
         }
-        
-        protected virtual void ProcessDrop()
-        {
-            
-        }
+
+        protected abstract void ProcessSingleDrop();
     }
 }
